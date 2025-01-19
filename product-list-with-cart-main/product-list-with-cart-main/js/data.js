d539da11d5dda67eece391b6f6eee510040b85c0
Teuku Neo <155743925+TeukuNeo@users.noreply.github.com>
2025-01-19 23:06:54 +0700

    Update data.js

--- product-list-with-cart-main/product-list-with-cart-main/js/data.js
+++ product-list-with-cart-main/product-list-with-cart-main/js/data.js
@@ -3,49 +3,49 @@ let generateShopItem = [{
     id: "WF",
     type: "Waffle",
     name: "Waffle with Berries",
-    img: "../assets/images/image-waffle-desktop.jpg",
+    img: "images/image-waffle-desktop.jpg",
     price: 6.50
 },{
     id: "CR",
     type: "Crème Brûlée",
     name: "Vanilla Bean Crème Brûlée",
-    img: "../assets/images/image-creme-brulee-desktop.jpg",
+    img: "images/image-creme-brulee-desktop.jpg",
     price: 7.00
 },{
     id: "MC",
     type: "Macaron",
     name: "Macaron Mix of Five",
-    img: "../assets/images/image-macaron-desktop.jpg",
+    img: "images/image-macaron-desktop.jpg",
     price: 8.00
 },{
     id: "CS",
     type: "Tiramisu",
     name: "Classic Tiramisu",
-    img: "../assets/images/image-tiramisu-desktop.jpg",
+    img: "images/image-tiramisu-desktop.jpg",
     price: 5.50
 },{
     id: "PS",
     type: "Baklava",
     name: "Pistachio Baklava",
-    img: "../assets/images/image-baklava-desktop.jpg",
+    img: "images/image-baklava-desktop.jpg",
     price: 4.00
 },{
     id: "LM",
     type: "Pie",
     name: "Lemon Meringue Pie",
-    img: "../assets/images/image-meringue-desktop.jpg",
+    img: "images/image-meringue-desktop.jpg",
     price: 5.00
 },{
     id: "RV",
     type: "Cake",
     name: "Red Velvet Cake",
-    img: "../assets/images/image-cake-desktop.jpg",
+    img: "images/image-cake-desktop.jpg",
     price: 4.50
 },{
     id: "SC",
     type: "Brownie",
     name: "Vanilla Bean Crème Brûlée",
-    img: "../assets/images/image-brownie-desktop.jpg",
+    img: "images/image-brownie-desktop.jpg",
     price: 4.50
 },{
     id: "VP",
